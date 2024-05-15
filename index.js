34366581737b6d4925c33619dd215cef7c199507
Ruben Salazar <rubensalazar@rocketmail.com>
2024-05-15 15:07:24 -0500

    funKctions continued

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+function saturdayFun (toDo = "roller-skate"){
+    return(`This Saturday, I want to ${toDo}!`)
+
+    }
+
+    saturdayFun('bathe my dog')
+
+function mondayWork (toDo = "go to the office"){
+    return(`This Monday, I will ${toDo}.`)
+}
+
+mondayWork('work from home')
+
+
+
+
+
+function wrapAdjective (visualFlair = "*"){
+
+    return function(adjective = "special"){
+        return(`You are ${visualFlair}${adjective}${visualFlair}!`)
+    }
+}
+
+
+
+
+
+
